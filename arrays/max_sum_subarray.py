fca69a588acf64ea49157a03b4a00a333dd2310f
innocentzv <innocent.zvovuranda@interactivesa.co.za>
2020-04-14 14:03:39 +0200

    added a script to update user acls

--- arrays/max_sum_subarray.py
+++ arrays/max_sum_subarray.py
@@ -1,5 +1,19 @@
+from functools import reduce
+
 arr = [1, 8, 6, 6, 1, 4, 3, 1, 0, 0, 7, 8]
 sum = 15
+
+# JavaScript Arrow Function
+#const square = number => number * number;
+#Python Lambda Expression
+square = lambda number: number * number
+
+# Map, Filter, and Reduce
+
+numbers = [1,2,3,4,5,6]
+odd_numbers = filter(lambda n: n % 2 == 1, numbers)
+squared_odd_numbers = map(lambda n: n * n, odd_numbers)
+total = reduce(lambda acc, n: acc + n, squared_odd_numbers)
 
 def find_max_sub_array(arr, val):
     left = 0
@@ -21,6 +35,8 @@ def find_max_sub_array(arr, val):
 
 
 print(find_max_sub_array(arr, sum))
+print(square(3))
+print(total)
 
 
  
